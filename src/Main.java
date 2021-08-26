1b2197e13ea1a79332caf471fe39c7e3ef82cc06
ilevann <neykuratick2@mail.ru>
2021-08-27 01:20:43 +0300

    Task 1 Done

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import DynamicArray.DynamicArray;
 public class Main {
 
     public static void main(String[] args) {
-        DynamicArray<Integer> array = new DynamicArray<Integer>(10);
-
-        array.set(4, 5);
-        array.set(6, 5);
-        array.set(5, 777);
-
-        array.printAll();
-        array.remove(5);
-        array.printAll();
+        // done
     }
 }
