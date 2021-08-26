322b16f1b91fbc377af075b8b1899fbb70c7b7aa
ilevann <neykuratick2@mail.ru>
2021-08-27 00:32:47 +0300

    init_tests

--- src/DynamicArray/DynamicArrayTests.java
+++ src/DynamicArray/DynamicArrayTests.java
@@ -1,4 +1,52 @@
 package DynamicArray;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class DynamicArrayTests {
+    // INIT TESTS
+    @Test
+    public void init_sizeDefault() {
+        DynamicArray<Integer> array = new DynamicArray<Integer>();
+
+        int expected = array.defaultSize;
+        int actual = array.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void init_SizeCustom() {
+        DynamicArray<Integer> array = new DynamicArray<Integer>(10);
+
+        int expected = 10;
+        int actual = array.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void init_InvalidSize_ThrowsException() {
+        assertThrows(InvalidArraySizeException.class, () -> new DynamicArray<Integer>(-1));
+        assertThrows(InvalidArraySizeException.class, () -> new DynamicArray<Integer>(0));
+    }
+
+    // GETSIZE TESTS
+
+    // GET TESTS
+
+    // FINDFIRST TESTS
+
+    // FINDLAST TESTS
+
+    // SET TESTS
+
+    // RESIZE TESTS
+
+    // APPEND TESTS
+
+    // INSERT TESTS
+
+    // REMOVE TESTS
+
 }
